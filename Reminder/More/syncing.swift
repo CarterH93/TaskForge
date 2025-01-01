4b239e32fcbaec3c3939f66224361700e812a620
CarterH93 <Carterhawkins93@gmail.com>
2025-01-01 15:44:51 -0500

    Reorganized syncing

--- Reminder/More/syncing.swift
+++ Reminder/More/syncing.swift
@@ -11,64 +11,9 @@ import SwiftData
 
 
 struct syncing: View {
-    @State private var deleteHaptic = false
-    @State private var link = ""
-    @State private var showingNewSheet = false
-    
-    @Environment(\.modelContext) var modelContext
-    @Query(
-        sort: \Settings1.Date1
-    ) var settings: [Settings1]
-    
-    @State private var localTempURLHold: [URL] = []
-    
-    func removeRows(at offsets: IndexSet) {
-        localTempURLHold = settings.first!.icsSources
-        localTempURLHold.remove(atOffsets: offsets)
-        settings.first!.icsSources.remove(atOffsets: offsets)
-        showingNewSheet = true
-        deleteHaptic.toggle()
-        }
-    
     var body: some View {
         List {
-            Section("Add .ICS Link") {
-                HStack {
-                    TextField("Paste Link Here...", text: $link)
-                    PasteButton(payloadType: String.self) { strings in
-                        guard let first = strings.first else { return }
-                        link = first
-                    }
-                    .buttonBorderShape(.capsule)
-                }
-                Button("Add Source") {
-                    
-                    if let url = URL(string: link) {
-                        //Do work
-                        localTempURLHold = settings.first!.icsSources
-                        localTempURLHold.append(url)
-                        settings.first!.icsSources.append(url)
-                        try? modelContext.save()
-                        showingNewSheet = true
-                    }
-                    link = ""
-                }
-            }
-            
-            Section {
-                ForEach(settings.first!.icsSources, id: \.self) { link in
-                    Text(link.description)
-                }
-                .onDelete(perform: removeRows)
-            } header: {
-                HStack {
-                    Text("Synced Sources")
-                    Spacer()
-                    EditButton()
-                        .font(.footnote)
-                }
-            }
-            
+            AddAndViewICSSources()
             Section {
                 NavigationLink("Clean up Spam") {
                    deleteListOfTasks()
@@ -76,11 +21,6 @@ struct syncing: View {
             }
             
         }
-        .sensoryFeedback(.warning, trigger: deleteHaptic)
-        .sheet(isPresented: $showingNewSheet) {
-            cleanUpSpam(localTempURLHold: localTempURLHold)
-                .presentationDragIndicator(.visible)
-                }
         .navigationTitle("Syncing")
     }
 }
