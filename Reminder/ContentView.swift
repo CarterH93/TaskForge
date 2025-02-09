aa41ccb1d42b4869cbf37909e9cfd83419d41ca1
CarterH93 <Carterhawkins93@gmail.com>
2025-02-08 20:07:08 -0500

    Fixed UI issues

--- Reminder/ContentView.swift
+++ Reminder/ContentView.swift
@@ -111,7 +111,6 @@ struct ContentView: View {
                                 
                                 let localNotification = LocalNotification(identifier: newReminder.id, categoryIdentifier: "reminderNotification", title: newReminder.name, userInfo: ["nextView" : newReminder.id], body: todayReminderBody(newReminder), dateComponents: dateComponents, repeats: false)
                                 
-                                print(localNotification)
                                 
                                 await lnManager.schedule(localNotification: localNotification)
                                 
